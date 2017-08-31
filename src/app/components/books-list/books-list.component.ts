8244035a9c0e65d9cfc9536e9b8cc6f52161b618
Stéphane Francel <contact@avine.io>
2017-08-31 16:19:56 +0200

    Add CartService in books-list component

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import { BooksModel } from '../../models/books.model';
+import { CartModel } from '../../models/cart.model';
+
 import { BooksService } from '../../services/books.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-books-list',
@@ -10,12 +13,14 @@ import { BooksService } from '../../services/books.service';
 })
 export class BooksListComponent implements OnInit {
   public books: BooksModel[] = [];
+  public cart: CartModel[] = [];
 
-  constructor(private booksService: BooksService) {
+  constructor(private booksService: BooksService, private cartService: CartService) {
   }
 
   ngOnInit() {
     this.booksService.list(books => this.books = books);
+    this.cartService.subscribe(cart => this.cart = cart); // TODO: Use this to ckeck the number of units for each book...
   }
 
 }
