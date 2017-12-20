e5bd146edf33633c353de8605704b29a057993af
Stéphane Francel <contact@avine.io>
2017-12-20 01:05:08 +0100

    Clean code...

--- src/app/testing/helper.ts
+++ src/app/testing/helper.ts
@@ -6,23 +6,18 @@ import { BookModel } from '../models/book.model';
 
 export class Fixture<T> {
   component: T;
-
   constructor(public fixture: ComponentFixture<T>) {
     this.component = this.fixture.componentInstance;
   }
-
   queryByCss(css) {
     return this.fixture.debugElement.query(By.css(css));
   }
-
   queryAllByCss(css) {
     return this.fixture.debugElement.queryAll(By.css(css));
   }
-
   queryByDirective(directive) {
     return this.fixture.debugElement.query(By.directive(directive));
   }
-
   queryAllByDirective(directive) {
     return this.fixture.debugElement.queryAll(By.directive(directive));
   }
@@ -32,12 +27,10 @@ export class Trigger {
   static click(de: DebugElement) {
     de.triggerEventHandler('click', null);
   }
-
   static inputText(de: DebugElement, value: string) {
     (de.nativeElement as HTMLInputElement).value = value;
     de.triggerEventHandler('input', null);
   }
-
   static checkboxChange(de: DebugElement, checked: boolean) {
     (de.nativeElement as HTMLInputElement).checked = checked;
     de.triggerEventHandler('change', null);
